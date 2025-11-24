77b30bd48c099821d18a82b78e9287beaef9385e
Haylee <shiqkoh13@gmail.com>
2025-11-24 22:36:53 +0800

    Enhance file reading with CSV support and word count
    Added CSV file handling and column selection for analysis.

--- stage 1.c
+++ stage 1.c
@@ -1,12 +1,12 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <direct.h> 
-#define BUFFER_SIZE 1000 
+#include <string.h>  
+#define BUFFER_SIZE 5000 
 
 int main() {
 
 	int numFiles;
-	int filesNumber = 0;
 	char fileName[260]; //260 is max path length in windows
 
 	printf("How many files do you want to read?\n");
@@ -29,33 +29,50 @@ int main() {
 		printf("\n--- Reading %s ---\n", fileName);
 
 		char buffer[BUFFER_SIZE]; //buffer is just a variable for an array of characters
-		// print content
-		while (fgets(buffer, sizeof(buffer), file) != NULL)  //Format of fgets(fgets(where to put the data, size, file_pointer) // ! = NULL means it has vaild variable
-		{
-			printf("%s", buffer); //%s means string //why print? showed that the reading wokred & you acn see what was read
+	
+
+		// Check csv files
+		char* extension = strrchr(fileName, '.');
+		int isCSV = (extension != NULL && strcmp(extension, ".csv") == 0);
+		int columnToRead = 0;
+
+		if (isCSV) {
+			printf("Which column number to analyze? (1, 2, 3, etc.): ");
+			scanf("%d", &columnToRead);
 		}
-
-		rewind(file); // Reset file pointer to beginning for word count
-
-		//Word Count// 
+	
+		// Reading and word counting
 		char ch;
 		int wordcount = 0;
 		int inword = 0;
-
-		while ((ch = fgetc(file)) != EOF)
+		int currentColumn = 1;
+		while ((ch = fgetc(file)) != EOF)  //Format of fgets(fgets(where to put the data, size, file_pointer) // ! = NULL means it has vaild variable
 		{
-			if (ch == ' ' || ch == '\n' || ch == '\t') //check for space, new line, tab
-			{
-				inword = 0;
-			}
+			printf("%c", ch); //%s means string //why print? showed that the reading wokred & you acn see what was read
 
-			else if (inword == 0)
-			{
-				inword = 1;
-				wordcount++;
-			}
-
+				if (isCSV && ch == ',') {
+					currentColumn++;  // Move to next column
+					inword = 0;
+				}
+				else if (ch == '\n') {
+					currentColumn = 1;  // New line, reset to column 1
+					inword = 0;
+				}
+				else if (isCSV && currentColumn != columnToRead) {
+					// Skip characters not in our target column
+					continue;
+				}
+				else if (ch == ' ' || ch == '\t') {
+					inword = 0;
+				}
+				else if (inword == 0) {
+					inword = 1;
+					wordcount++;
+				}
+			
 		}
+
+		
 		printf("\nTotal words: %d\n", wordcount);
 
 		fclose(file); //close the file
